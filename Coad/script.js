d68c7e3768c431c1df6a45fb8a98c077a76c92ce
ShawonDeb <68333646+ShawonDeb@users.noreply.github.com>
2020-09-22 00:15:52 +0600

    Add files via upload

---
+++ Coad/script.js
@@ -0,0 +1,4 @@
+alert("Hi Welcome To My Page..");
+console.log("This page is made for practice");
+console.log("If i got any mistrac..Please forgive me..😊😊");
+console.log("I make tis site for computer.. It's not responsive. So If you usc Mobile or Tab This website is not loking good..Please Don't mind..Sorry");
